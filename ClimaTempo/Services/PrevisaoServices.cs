0c5bb7dc21a999cb8f229dd7e11da0846a61d36f
Danillo de Lacerda <danillo.lacerda77@gmail.com>
2024-11-29 19:49:25 -0300

    Criação das classes e dos métodos da previsão

---
+++ ClimaTempo/Services/PrevisaoServices.cs
@@ -0,0 +1,71 @@
+﻿using ClimaTempo.Models;
+using System.Diagnostics;
+using System.Text.Json;
+
+namespace ClimaTempo.Services
+{
+    public class PrevisaoServices
+    {
+        private HttpClient client;
+        private Previsao previsao;
+        private Previsao previsaoProximosDias;
+        private JsonSerializerOptions options;
+        Uri uri = new Uri("https://brasilapi.com.br/api/cptec/v1/clima/previsao/");
+
+        public PrevisaoServices()
+        {
+            client = new HttpClient();
+            options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            };
+        }
+
+        public async Task<Previsao> GetPrevisaoById(int cityCode)
+        {
+            cityCode = 244;
+            Uri requestUri = new Uri($"{uri}/{cityCode}");
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(requestUri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    previsao = JsonSerializer.Deserialize<Previsao>(content, options);
+                };
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return previsao;
+        }
+
+        public async Task<Previsao> GetPrevisaoForXDaysById(int cityCode, int days)
+        {
+
+            try
+            {
+                cityCode = 244;
+                days = 3;
+                Uri requestUri = new Uri($"{uri}/{cityCode}/{days}");
+
+
+                HttpResponseMessage response = await client.GetAsync(requestUri);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    previsaoProximosDias = JsonSerializer.Deserialize<Previsao>(content, options);
+                };
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return previsaoProximosDias;
+        }
+
+    }
+}
+
---
+++ ClimaTempo/ViewModels/PrevisaoViewModel.cs
@@ -0,0 +1,100 @@
+﻿using ClimaTempo.Models;
+using ClimaTempo.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ClimaTempo.ViewModels
+{
+    public partial class PrevisaoViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string cidade;
+        
+        [ObservableProperty]
+        private string estado;
+
+        [ObservableProperty]
+        private string condicao;
+
+        [ObservableProperty]
+        private string condicao_desc;
+
+        [ObservableProperty]
+        private DateTime data;
+        
+        [ObservableProperty]
+        private double min;
+
+        [ObservableProperty]
+        private double max;
+
+        [ObservableProperty]
+        private double indice_uv;
+
+        [ObservableProperty]
+        private List<Clima> proximosDias;
+
+        private Previsao previsao;
+        private Previsao proxPrevisao;
+
+
+        // Dados da Pesquisa
+        [ObservableProperty]
+        private string cidade_pesquisada;
+
+
+        // Dados da lista de Cidade
+        [ObservableProperty]
+        private List<Cidade> cidade_list;
+
+        public ICommand BuscarPrevisaoCommand { get; }
+
+        public ICommand BuscarCidadesCommand { get; }
+
+
+        public PrevisaoViewModel()
+        {
+            BuscarPrevisaoCommand = new Command(BuscarPrevisao);
+            BuscarCidadesCommand = new Command(BuscarCidade);
+        }
+
+        public async  void BuscarPrevisao()
+        {
+            //Busca os dados da previsão para uma cidade especificada:
+            previsao = await new PrevisaoServices().GetPrevisaoById(244);
+            
+            Cidade = previsao.Cidade;
+            Estado = previsao.Estado;
+            Condicao = previsao.Clima[0].Condicao;
+            Condicao_desc = previsao.Clima[0].Condicao_desc;
+
+            Min = previsao.Clima[0].Min;
+            Max = previsao.Clima[0].Max;
+            Indice_uv = previsao.Clima[0].Indice_uv;
+            Data = previsao.Clima[0].Data;
+
+
+            proxPrevisao = await new PrevisaoServices().GetPrevisaoForXDaysById(244, 3);
+            ProximosDias = proxPrevisao.Clima;
+           
+            
+
+            //Busca os dados da previsão para os próximos dias:
+            proxPrevisao = await new PrevisaoServices().GetPrevisaoForXDaysById(244, 3);
+            ProximosDias = proxPrevisao.Clima;
+        }
+    
+        public async void BuscarCidade()
+        {
+            Cidade_list = new List<Cidade>();
+            Cidade_list = await new CidadeService().GetCidadesByName(Cidade_pesquisada);
+        }
+    
+    
+    }
+}
